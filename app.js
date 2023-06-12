25391078f8ae2e31e6c9fbd684c4bf2dcc93dfff
Cátia Brehm <catiabrehm@MBP-de-Catia.home>
2023-06-12 12:58:58 +0100

    final changes

--- app.js
+++ app.js
@@ -2,7 +2,14 @@ let toDoButton = document.getElementById('addButton');
 let addToDos = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 
+toDoButton.addEventListener('click', function(){
 
+    let paragraph = document.createElement('p');
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = '';
 
-
-
+    paragraph.addEventListener('click', function(){
+    toDoContainer.removeChild(paragraph);
+    })
+})
